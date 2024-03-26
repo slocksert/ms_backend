4ce6f6cfd5905f912dd9ba01335f75cc218ee15f
slocksert <rsldj01@gmail.com>
2024-03-26 20:40:48 -0300

    server file

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from decouple import config
+
+from routes import register, login, index, adm_route
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#routes
+app.include_router(register)
+app.include_router(login)
+app.include_router(index)
+app.include_router(adm_route)
+
+#runs the api
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=config("host"),
+        port=int(config("port")),
+        reload=1,
+        server_header=0
+    )
